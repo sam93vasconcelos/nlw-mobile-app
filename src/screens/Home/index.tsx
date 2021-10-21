2607f231fd07bbdc9cdcd7f806a43562ac5b3c67
= <=>
2021-10-21 01:13:08 -0300

    Create send message component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
+import { SendMessageForm } from '../../components/SendMessageForm';
 import { SignInBox } from '../../components/SignInBox';
 
 import { styles } from './styles';
@@ -12,7 +13,8 @@ export function Home() {
 		<View style={styles.container}>
 			<Header />
 			<MessageList />
-			<SignInBox />
+			<SendMessageForm />
+			{/* <SignInBox /> */}
 		</View>
 	);
 }
