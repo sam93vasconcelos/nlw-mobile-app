b587a251691b498967a5ae2960153b90b80df349
= <=>
2021-10-21 11:59:45 -0300

    Load messages in realtime

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -4,7 +4,17 @@ import { ScrollView } from 'react-native';
 import { api } from '../../services/api';
 import { Message, MessageProps } from '../Message';
 
+import { io } from 'socket.io-client';
+
 import { styles } from './styles';
+
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+
+socket.on('new_message', newMessage => {
+  messagesQueue.push(newMessage);
+});
 
 export function MessageList() {
   const [messages, setMessages] = useState<MessageProps[]>([]);
@@ -17,6 +27,23 @@ export function MessageList() {
 
     fetchMessages();
   },[])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if(messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return ()  => {
+      clearInterval(timer);
+    }
+  }, []);
 
 	return (
     <ScrollView 
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, Text, View } from 'react-native';
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
 import { SendMessageForm } from '../../components/SendMessageForm';
@@ -13,10 +13,15 @@ export function Home() {
 	const { user } = useAuth();
 
 	return (
-		<View style={styles.container}>
-			<Header />
-			<MessageList />
-			{ user ? <SendMessageForm /> : <SignInBox />}
-		</View>
+		<KeyboardAvoidingView
+			style={{flex: 1}}
+			behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+		>
+			<View style={styles.container}>
+				<Header />
+				<MessageList />
+				{ user ? <SendMessageForm /> : <SignInBox />}
+			</View>
+		</KeyboardAvoidingView>
 	);
 }
